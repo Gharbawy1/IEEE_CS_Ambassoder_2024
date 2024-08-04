d6bd728e5b8e57f21251535de30171679192a62b
Ahmed El-gharbawy <136514345+Gharbawy1@users.noreply.github.com>
2024-08-05 02:25:14 +0300

    Create A. Hulk.cpp

---
+++ Rookies_Tasks/Task3/A. Hulk.cpp
@@ -0,0 +1,27 @@
+// problem link : https://codeforces.com/problemset/problem/705/A
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<set>
+#include<vector>
+#include<map>
+#include<cmath>
+using namespace std;
+ 
+#define MASR ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define sp ' ';
+#define ll long long
+const long long oo = 1e18;
+const int N1 = 1e3;
+void Gamila(){
+
+
+}
+
+int main(){
+  MASR 
+  Gamila();
+  
+
+
+}
