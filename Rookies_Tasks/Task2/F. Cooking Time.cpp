9d3ebeb5499c27adcffff2a8747a9e941a5fc9a9
Ahmed El-gharbawy <136514345+Gharbawy1@users.noreply.github.com>
2024-08-06 00:46:27 +0300

    Create F. Cooking Time.cpp

---
+++ Rookies_Tasks/Task2/F. Cooking Time.cpp
@@ -0,0 +1,5 @@
+// problem link : https://codeforces.com/gym/101498/problem/F
+// note that O(N^2) is time limit 
+// hint1 : think about suit DataStructre 
+// hint2 : think about how to choose the most efficient element that U need to put in the frideg to get another one ! (this is the most critical part in the problem)
+// (shit for this problem)
